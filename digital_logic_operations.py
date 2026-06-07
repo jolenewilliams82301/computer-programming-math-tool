b56633e40fe755932ae95beff5db56ed2ac1e0be
jolenewilliams82301 <williamsjolene823@gmail.com>
2026-06-06 19:36:37 -0700

    add digital logic operations file

---
+++ digital_logic_operations.py
@@ -0,0 +1,11 @@
+"""
+File: digital_logic_operations.py
+Author: Jolene Williams
+Description: Operations relating to digital logic: hex/binary/octal/decimal conversions, 
+bitwise operations, binary addition
+
+NCLab Capstone Project 2
+
+Notes:
+
+"""
